b2b5c809aba2f97206b2e43131dc0bc15cad373b
rpalmer0812 <jchou0812@gmail.com>
2020-06-05 07:34:11 +1000

    Sending Emails

---
+++ Email.py
@@ -0,0 +1,13 @@
+import yagmail
+
+sender_email = 'rpalmer1502@gmail.com'
+receiver_email = 'alcyonmonocero@gmail.com'
+subject = "Check THIS out"
+sender_password = input(f'Please, enter the password for {sender_email}:\n')
+yag = yagmail.SMTP(user=sender_email, password=sender_password)
+contents = [
+  "blhablhablahbalhbalbhalbhabhblahblhba",
+  "As you can see, we can send a list of strings,",
+  "being this our third one",
+]
+yag.send(receiver_email, subject, contents)
---
+++ Testfile.py
@@ -0,0 +1,5 @@
+contents = [
+from Lottery import *
+]
+
+print(contents)
