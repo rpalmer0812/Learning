dada1381c3d03fb4c093f62f63a60e54d254f82f
rpalmer0812 <jchou0812@gmail.com>
2020-06-12 08:58:57 +1000

    alien_invasion game

--- Alien_Invasion/alien_invasion.py
+++ Alien_Invasion/alien_invasion.py
@@ -1,12 +1,18 @@
 import sys
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 class AlienInvasion:
     def __init__(self):
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(self.settings.screen_width, self.settings.screen_height)
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
 
         # Set Background Colour
         self.bg_color = (230, 230, 230)
@@ -17,7 +23,9 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             # Redraw the screen during each pass throught he loop
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+            # Make the most recently drawn screen visible
             pygame.display.flip()
 
 if __name__ == '__main__':
