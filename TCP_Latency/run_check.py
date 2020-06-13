7d62b5ddbc994f7f4b3f84cf2c10a29c9cc9c954
rpalmer0812 <jchou0812@gmail.com>
2020-06-14 01:21:38 +1000

    tcp_latency

--- TCP_Latency/run_check.py
+++ TCP_Latency/run_check.py
@@ -1,1 +1,62 @@
 from tcp_latency import *
+import subprocess
+import re
+import statistics
+
+process = subprocess.run('python tcp_latency.py --run 50 8.8.8.8', shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
+output = process.stdout
+
+
+final = re.findall(r'(?<=time=)[0-9.]+', output)
+
+#print(final)
+
+new_final = [float(i) for i in final]
+#print(new_final)
+
+#def toInt(lst):
+#    for i in lst:
+#        new_final = float(final.pop(i))
+#    return new_final
+
+#new_list = toInt(final)
+#print(new_list)
+
+def Average(lst):
+    sum = 0
+    for i in lst:
+        sum += float(i)
+    avg = sum / len(lst)
+    return avg
+
+#def median_value(lst):
+#    int_list = []
+#    for i in lst:
+#        int_list.append(float(lst[i]))
+#    return int_list
+
+#m_value = median_value(final)
+#print(m_value)
+
+max_ping = round(float(max(new_final)))
+min_ping = round(float(min(new_final)))
+avg_ping = round(float(Average(new_final)))
+
+print(f"\nMaximum ping: {max_ping}")
+print(f"Minimum ping: {min_ping}")
+print(f"\nAverage ping: {avg_ping}")
+
+if avg_ping < 20 and max_ping < 25:
+    print("Internet is looking good.\n")
+elif avg_ping < 20 and max_ping > 24:
+    print("Internet is generally smooth with some odd spikes.\n")
+elif avg_ping > 39 and max_ping > 24:
+    print("Internet is congested.\n")
+elif avg_ping > 19 and max_ping > 24:
+    print("Internet is slightly congested and only slightly impacted.\n")
+
+#Regex.Match(@"""id"":143331539043251,", @"""id"":([0-9]+),").Groups[1].Value
+
+#ping_output = subprocess.run('python tcp_latency.py --run 5 8.8.8.8', shell=True)
+
+#print(ping_output)
